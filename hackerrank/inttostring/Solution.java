f7786b2097450cb0470dd7d9b329aa435e4ccc91
Marcos <marcos.yuriro@gmail.com>
2026-08-16 11:38:46 -0300

    updates int to string solution

--- hackerrank/inttostring/Solution.java
+++ hackerrank/inttostring/Solution.java
@@ -5,14 +5,13 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        try {
+        try (Scanner scanner = new Scanner(System.in)) {
             int number = scanner.nextInt();
             String numberToString = String.valueOf(number);
             System.out.println("Good job");
         } catch(Exception e) {
             System.out.println("Wrong answer");
-        }
+        } 
     }
 }   
